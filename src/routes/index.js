faf824b326d21cf2493a2c33fd597825f6d6601f
haotranhuynhhuy <tranhuynhhuyhao@gmail.com>
2022-07-15 13:56:08 +0700

    Bai 62: Cấu hình Router/Layout cho dự án

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import Home from "../pages/Home";
+import Following from "../pages/Following";
+import Profile from "../pages/Profile";
+import Upload from "../pages/Upload";
+import { HeaderOnlyLaybout } from "../components/Layout";
+const publicRoutes = [
+  { path: "/", component: Home },
+  { path: "/following", component: Following },
+  { path: "/profile", component: Profile, layout: null },
+  { path: "/upload", component: Upload, layout: HeaderOnlyLaybout },
+];
+const privateRoutes = [];
+
+export { publicRoutes };
